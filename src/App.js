6b51d4d9c507a0423271246b2e2a66d3c3a81a39
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-03 14:46:13 +0300

    cleaning the app to get started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Spotify</h1>
     </div>
   );
 }
 
 export default App;
+//Client ID 460b84ef0d054df58992bea1f4ab54cc
